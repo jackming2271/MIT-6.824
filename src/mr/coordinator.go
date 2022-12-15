42a5f4ba195726887b8dae33d2004be2bac4b0a0
jackming2271 <jackming2271@gmail.com>
2022-12-15 21:29:00 +0800

    impl tiny mapreduce

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,164 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"strconv"
+	"sync"
+)
 
 type Coordinator struct {
 	// Your definitions here.
-
+	stage             int
+	nDistributeMap    int
+	nUnFinishedMap    int
+	nDistributeReduce int
+	nUnFinishedReduce int
+	nMap              int
+	nReduce           int
 }
 
-// Your code here -- RPC handlers for the worker to call.
+var coordinatorMutex sync.Mutex
 
-//
+// Your code here -- RPC handlers for the worker to call.
+// @brief 切分所有input为num_mapper个输入文件
+// @param files
+// @param num_mapper
+// @return error
+func split(files []string) (int, error) {
+	num_mapper := 0
+	size_sum := 0
+	for _, file := range files {
+		fileInfo, err := os.Stat(file)
+		if err != nil {
+			log.Fatal(err)
+			return -1, err
+		}
+		size_sum += int(fileInfo.Size())
+		fmt.Printf("%v %v\n", file, size_sum)
+	}
+	num_mapper = (size_sum)/262144 + 1
+	if num_mapper <= 0 {
+		return -1, errors.New("num_mapper error")
+	}
+
+	// create scanners
+	scanners := make([]*bufio.Scanner, len(files))
+	for index, file := range files {
+		fd, err := os.Open(file)
+		if err != nil {
+			log.Fatal(err)
+			return -1, err
+		}
+		scanner := bufio.NewScanner(fd)
+		scanners[index] = scanner
+	}
+
+	// open outout_file
+	outers := make([]*os.File, num_mapper)
+	for i := 0; i < num_mapper; i++ {
+		file, err := os.OpenFile(
+			"../mr-tmp/split-"+strconv.Itoa(i+1),
+			os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+			0666,
+		)
+		if err != nil {
+			log.Fatal(err)
+			return -1, err
+		}
+		outers[i] = file
+	}
+
+	//
+	index_output_file := 0
+	for {
+		split_end := true
+		for _, scanner := range scanners {
+			if scanner.Scan() {
+				split_end = false
+				outers[index_output_file].Write(scanner.Bytes())
+				outers[index_output_file].Write([]byte("\n"))
+				index_output_file++
+				index_output_file %= len(outers)
+			}
+		}
+		if split_end {
+			break
+		}
+	}
+
+	// close input&output fd
+	for _, fd := range outers {
+		fd.Close()
+	}
+
+	return num_mapper, nil
+}
+
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
+func (c *Coordinator) Handle(request *CoordinatorRequest, response *CoordinatorResponse) error {
+	coordinatorMutex.Lock()
+	defer coordinatorMutex.Unlock()
+	response.NMap = c.nMap
+	response.NReduce = c.nReduce
 
-//
+	switch request.Action {
+	case 0:
+		if c.nDistributeMap > 0 && c.stage == 1 {
+			response.Id = c.nDistributeMap
+			response.Stage = c.stage
+			c.nDistributeMap -= 1
+		} else if c.nDistributeReduce > 0 && c.stage == 2 {
+			response.Id = c.nDistributeReduce
+			response.Stage = c.stage
+			c.nDistributeReduce -= 1
+		} else {
+			response.Id = 0
+			response.Stage = c.stage
+			return errors.New("")
+		}
+
+	case 1:
+		if c.stage == 1 {
+			c.nUnFinishedMap -= 1
+			if c.nUnFinishedMap == 0 {
+				c.stage = 2
+			}
+		} else if c.stage == 2 {
+			c.nUnFinishedReduce -= 1
+			if c.nUnFinishedReduce == 0 {
+				c.stage = 3
+			}
+		} else {
+			response.Id = -1
+			response.Stage = c.stage
+		}
+
+	default:
+		response.Id = -1
+		response.Stage = c.stage
+		return errors.New("Error Action")
+	}
+
+	// fmt.Printf("[master] [Action = %v][stage = %v] [nDistributeMap = %v] [nUnFinishedMap = %v] [nDistributeReduce = %v] [nUnFinishedReduce = %v]\n",
+	// 	request.Action, c.stage, c.nDistributeMap, c.nUnFinishedMap, c.nDistributeReduce, c.nUnFinishedReduce)
+	return nil
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +172,42 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
-	return ret
+	coordinatorMutex.Lock()
+	defer coordinatorMutex.Unlock()
+	if c.stage != 3 {
+		return false
+	}
+	return true
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
-// nReduce is the number of reduce tasks to use.
-//
-func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+// nDistributeReduce is the number of reduce tasks to use.
+func MakeCoordinator(files []string, nDistributeReduce int) *Coordinator {
 
 	// Your code here.
+	// split
 
+	nDistributeMap, err := split(files)
+	fmt.Printf("nDistributeMap : %v", nDistributeMap)
+	if err != nil {
+		log.Fatal("split error: ", err)
+		return nil
+	}
+	// init Coordinator
+	c := Coordinator{
+		1,
+		nDistributeMap,
+		nDistributeMap,
+		nDistributeReduce,
+		nDistributeReduce,
+		nDistributeMap,
+		nDistributeReduce,
+	}
+	//map_stage
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,48 +1,132 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"bufio"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
 
 	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+	for true {
+		response := RequestTask()
+		// fmt.Printf("response.Stage = %v\n", response.Stage)
+		if (response.Stage != 3) && (response.Id <= 0) {
+			time.Sleep(1000 * time.Millisecond)
+			continue
+		} else if response.Stage == 3 {
+			return
+		}
+
+		switch response.Stage {
+		case 1:
+			// open outout_file
+			outers := make([]*os.File, response.NReduce)
+			for i := 0; i < response.NReduce; i++ {
+				file, err := os.OpenFile(
+					"../mr-tmp/mr-"+strconv.Itoa(response.Id)+"-"+strconv.Itoa(i+1),
+					os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+					0666,
+				)
+				if err != nil {
+					log.Fatal(err)
+					return
+				}
+				outers[i] = file
+			}
+			// 读入并统计
+			inputFilePath := "../mr-tmp/split-" + strconv.Itoa(response.Id)
+			fd, err := os.Open(inputFilePath)
+			if err != nil {
+				log.Fatal(err)
+			}
+			// scanner := bufio.NewScanner(fd)
+			content, err := ioutil.ReadAll(fd)
+			if err != nil {
+				log.Fatalf("cannot read %v", fd)
+			}
+			// fmt.Printf("content :  %v", string(content))
+			kva := mapf("filename", string(content))
+			// fmt.Printf("kva: %v\n", kva)
+			wordsMap := make(map[string]int)
+			for i := 0; i < len(kva); i++ {
+				// index := ihash(keyValues[i].Key) % response.NReduce
+				wordsMap[kva[i].Key] += 1
+				// outers[index].Write()
+			}
+			// 写入中间文件
+			for key, value := range wordsMap {
+				index := ihash(key) % response.NReduce
+				outers[index].Write([]byte(key + " " + strconv.Itoa(value) + "\n"))
+			}
+			// 回复success 给master
+			ResponseTask(response.Id)
+		case 2:
+			// 汇聚
+			wordsMap := make(map[string]int)
+			for i := 0; i < response.NMap; i++ {
+				inputFilePath := "../mr-tmp/mr-" + strconv.Itoa(i+1) + "-" + strconv.Itoa(response.Id)
+				fd, err := os.Open(inputFilePath)
+				if err != nil {
+					log.Fatal(err)
+				}
+				scanner := bufio.NewScanner(fd)
+				for scanner.Scan() {
+					key := strings.Split(scanner.Text(), " ")[0]
+					value := strings.Split(scanner.Text(), " ")[1]
+					num, _ := strconv.Atoi(value)
+					wordsMap[key] += num
+				}
+			}
+			// 写输出文件
+			onputFilePath := "../mr-tmp/mr-out-" + strconv.Itoa(response.Id)
+			file, err := os.OpenFile(
+				onputFilePath,
+				os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+				0666,
+			)
+			if err != nil {
+				log.Fatal("error open output_reduce_file")
+			}
+			for key, value := range wordsMap {
+				file.Write([]byte(key + " " + strconv.Itoa(value) + "\n"))
+			}
+			// 回复success 给master
+			ResponseTask(response.Id)
+		}
+	}
 
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -58,20 +142,46 @@ func CallExample() {
 	// the "Coordinator.Example" tells the
 	// receiving server that we'd like to call
 	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
+	call("Coordinator.Example", &args, &reply)
+}
+
+func RequestTask() CoordinatorResponse {
+
+	// declare an argument structure.
+	request := CoordinatorRequest{}
+	request.Action = 0
+	// fill in the argument(s).
+	// args.X = 99
+
+	// declare a reply structure.
+	response := CoordinatorResponse{}
+
+	// send the RPC request, wait for the reply.
+	// the "Coordinator.Example" tells the
+	// receiving server that we'd like to call
+	// the Example() method of struct Coordinator.
+	call("Coordinator.Handle", &request, &response)
+	return response
+}
+
+func ResponseTask(id int) {
+	request := CoordinatorRequest{}
+	request.Action = 1
+	request.Id = id
+
+	response := CoordinatorResponse{}
+
+	ok := call("Coordinator.Handle", &request, &response)
 	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
+		fmt.Printf("response.stage %v, response.id %v\n", response.Stage, response.Id)
 	} else {
 		fmt.Printf("call failed!\n")
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
